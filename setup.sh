2ddc481817132b300d92bc3e7755e2516c78ab5b
Jaime Landers <jaimelanders@gmail.com>
2022-06-11 20:17:25 -0700

    Fix install docker-compose

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
             $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo docker-compose scale app=2 nginx=1
+sudo apt install docker-compose
